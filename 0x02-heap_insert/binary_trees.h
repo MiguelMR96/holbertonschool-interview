e8cf88ddcfc0172ba0a25c3b0ac9a27867de6416
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2021-05-06 23:00:58 -0500

    Task 1

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -26,4 +26,22 @@ typedef struct binary_tree_s binary_tree_t;
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+/**
+ * struct space_s - Struct to find the space at a Binary tree node.
+ * @n: Size of the node
+ * @p: Node.
+ */
+struct space_s
+{
+	int n;
+	heap_t *p;
+};
+
+typedef struct space_s space_t;
+
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *insert_new_node(heap_t *parent);
+heap_t *heap_swap(heap_t *node);
+space_t heap_size(heap_t *node);
+
 #endif /* BINARY_TREES_H */
